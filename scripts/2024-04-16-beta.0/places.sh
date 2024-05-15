aa02b3007c82f53cfbe1fc75fe7beb5bc486bd96
Brandon Liu <bdon@bdon.org>
2024-05-16 00:15:36 +0800

    update places script

--- scripts/2024-04-16-beta.0/places.sh
+++ scripts/2024-04-16-beta.0/places.sh
@@ -7,8 +7,25 @@ COPY (
     SELECT
         'Feature' AS type,
         json(st_asgeojson(st_geomfromwkb(geometry))) AS geometry,
-        json_object() AS properties 
-    FROM read_parquet('s3://overturemaps-us-west-2/release/2024-04-16-beta.0/theme=places/type=place/*') 
+        json_object(
+            'id', id,
+            '@name', json_extract_string(names, '$.primary'),
+            '@category', json_extract_string(categories, '$.main'),
+            'names', names,
+            'confidence', confidence,
+            'categories', categories,
+            'websites', websites,
+            'socials', socials,
+            'emails', emails,
+            'phones', phones,
+            'brand', brand,
+            'addresses', addresses,
+            'version', version,
+            'update_time', update_time,
+            'sources', sources
+        ) AS properties,
+        row_number() over () as id,
+    FROM read_parquet('/srv/data/overture/2024-04-16-beta.0/theme=places/type=place/*') 
 ) TO STDOUT (FORMAT json);
 " | tippecanoe -o $1 --force -j '{ "*": [ "attribute-filter", "name", [ ">=", "$zoom", 9 ] ] }' -l places -rg --drop-densest-as-needed
 
