53e06dba8f1e8a114ea1574ad0f87c83afe4d845
Brandon Liu <bdon@bdon.org>
2024-05-28 15:15:39 +0800

    add all types to may Base

--- scripts/2024-05-16-beta.0/Base.java
+++ scripts/2024-05-16-beta.0/Base.java
@@ -7,18 +7,55 @@ public class Base implements OvertureProfile.Theme {
   @Override
   public void processFeature(SourceFeature source, FeatureCollector features) {
     String layer = source.getSourceLayer();
+    String clazz = source.getString("class");
     if (layer.equals("infrastructure")) {
-      var feature = OvertureProfile.createAnyFeature(source, features);
-      feature.setMinZoom(9);
+      feature.setMinZoom(13);
       OvertureProfile.addFullTags(source, feature);
     } else if (layer.equals("land")) {
+      int minzoom = 7;
+      if (source.isPoint()) {
+        minzoom = 13;
+      } else if (clazz.equals("land") || clazz.equals("glacier")) {
+        minzoom = 0;
+      }
+      feature.setMinZoom(minzoom);
+      OvertureProfile.addFullTags(source, feature);
     } else if (layer.equals("land_use")) {
-
+      int minzoom = 9;
+      if (source.isPoint()) {
+        minzoom = 13;
+      } else if (clazz.equals("residential")) {
+        minzoom = 6;
+      }
+      feature.setMinZoom(minzoom);
+      OvertureProfile.addFullTags(source, feature);
     } else if (layer.equals("land_cover")) {
       var feature = OvertureProfile.createAnyFeature(source, features);
+      var cartography = source.getStruct("cartography");
+      feature.setMaxZoom(cartography.get("max_zoom").asInt());
+      feature.setMinZoom(cartography.get("min_zoom").asInt());
       OvertureProfile.addFullTags(source, feature);
     } else if (layer.equals("water")) {
-
+      int minzoom = 13;
+      if (source.isPoint()) {
+        if (clazz.equals("ocean")) {
+          minzoom = 0;
+        } else {
+          minzoom = 8;
+        }
+      } else {
+        if (clazz.equals("lake") || clazz.equals("ocean") || clazz.equals("reservoir")) {
+          minzoom = 4;
+        } else if (clazz.equals("river")) {
+          minzoom = 9;
+        } else if (clazz.equals("canal")) {
+          minzoom = 12;
+        } else if (clazz.equals("stream")) {
+          minzoom = 13;
+        }
+      }
+      feature.setMinZoom(minzoom);
+      OvertureProfile.addFullTags(source, feature);
     }
   }
 
