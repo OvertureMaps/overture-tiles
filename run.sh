35f262ee7a5b80b9a051e9c053e8bd791f68ebfb
Brandon Liu <bdon@bdon.org>
2024-06-25 16:54:57 +0200

    fix unbound variable

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ RELEASE_DATA="2024-06-13-beta.1"
 # trim the patch version: 2024-06-13-beta.1 -> 2024-06-13-beta
 RELEASE_TILESET="${RELEASE_DATA%%.*}"
 
-aws s3 sync --no-progress --region us-west-2 --no-sign-request s3://overturemaps-us-west-2/release/$RELEASE/theme=$THEME /data/theme=$THEME
+aws s3 sync --no-progress --region us-west-2 --no-sign-request s3://overturemaps-us-west-2/release/$RELEASE_DATA/theme=$THEME /data/theme=$THEME
 
 if [ "$THEME" == "places" ] || [ "$THEME" == "divisions" ]; then
   bash scripts/$RELEASE_TILESET/$THEME.sh $THEME.pmtiles
