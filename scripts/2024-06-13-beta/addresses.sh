3811ccfaf02056be8a6c336ab25108ceb37afb4e
Brandon Liu <bdon@bdon.org>
2024-07-13 11:32:55 +0200

    Add addresses scripts

---
+++ scripts/2024-06-13-beta/addresses.sh
@@ -0,0 +1,27 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+set -e
+set -u
+set -o pipefail
+duckdb -c "
+load spatial;
+
+COPY ( 
+    SELECT
+        'Feature' AS type,
+        json(st_asgeojson(st_geomfromwkb(geometry))) AS geometry,
+        json_object(
+            'id', id,
+            'street', street,
+            'number', number,
+            'unit', unit,
+            'postcode', postcode,
+            'address_levels', address_levels,
+            'country', country,
+            'version', version,
+            'sources', sources
+        ) AS properties,
+        row_number() over () as id,
+    FROM read_parquet('$1/theme=addresses/type=address/*')
+) TO STDOUT (FORMAT json);
+" | tippecanoe -o $2 --force -J $SCRIPT_DIR/addresses.filter.json -l address -rg --drop-densest-as-needed --extend-zooms-if-still-dropping --progress-interval=10
