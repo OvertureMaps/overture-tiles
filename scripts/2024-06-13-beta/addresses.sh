4cc7d1a02d1023b0d011e4208adca218f52cdf0e
Brandon Liu <bdon@bdon.org>
2024-07-13 22:35:18 +0200

    fix addresses

--- scripts/2024-06-13-beta/addresses.sh
+++ scripts/2024-06-13-beta/addresses.sh
@@ -24,4 +24,4 @@ COPY (
         row_number() over () as id,
     FROM read_parquet('$1/theme=addresses/type=address/*')
 ) TO STDOUT (FORMAT json);
-" | tippecanoe -o $2 --force -J $SCRIPT_DIR/addresses.filter.json -l address -rg --drop-densest-as-needed --extend-zooms-if-still-dropping --progress-interval=10
+" | tippecanoe -o $2 --force -J $SCRIPT_DIR/addresses.filter.json -l address -rg --drop-densest-as-needed  --maximum-tile-bytes=1000000 --extend-zooms-if-still-dropping --progress-interval=10
