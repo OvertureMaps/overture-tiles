f0138c39963002eef23222f315c7636f5c5f62c7
Benjamin Clark <clarkben@meta.com>
2025-03-21 14:31:34 -0400

    Update QA run script to accept an arbitrary src/dest bucket folder.

--- run.sh
+++ run.sh
@@ -5,8 +5,9 @@ set -x
 # Automation script for running inside Docker on AWS Batch.
 
 RELEASE_DATA=$1
-BUCKET=$2
-THEME=$3
+SRC_BUCKET_FOLDER=$2
+DEST_BUCKET_FOLDER=$3
+THEME=$4
 
 # The most recent major version used in the /scripts directory
 SCRIPTS_VERSION="2024-07-22"
@@ -15,21 +16,15 @@ SCRIPTS_VERSION="2024-07-22"
 RELEASE_TILESET="${RELEASE_DATA%%.*}"
 
 # Download the full theme to /data.
-aws s3 sync --no-progress --region us-west-2 --no-sign-request s3://overturemaps-us-west-2/release/$RELEASE_DATA/theme=$THEME /data/theme=$THEME
+aws s3 sync --no-progress --region us-west-2 --no-sign-request s3://$SRC_BUCKET_FOLDER/ /data/theme=$THEME
 
 # Tile and upload the theme to the target bucket.
 if [ "$THEME" == "admins" ] || [ "$THEME" == "places" ] || [ "$THEME" == "divisions" ]; then
-  # Target a specific set of release scripts for generating tiles
-  if test -d scripts/$RELEASE_TILESET/$THEME.sh; then
-    bash scripts/$RELEASE_TILESET/$THEME.sh /data $THEME.pmtiles
-  # Generate tiles using the latest release scripts
-  else
-    bash scripts/$SCRIPTS_VERSION/$THEME.sh /data $THEME.pmtiles
-  fi
+  bash scripts/$SCRIPTS_VERSION/$THEME.sh /data $THEME.pmtiles
 
-  aws s3 cp --no-progress $THEME.pmtiles s3://$BUCKET/$RELEASE_TILESET/$THEME.pmtiles
+  aws s3 cp --no-progress $THEME.pmtiles s3://$DEST_BUCKET_FOLDER/$THEME.pmtiles
 else
   className="${THEME^}"
   java -cp planetiler.jar /profiles/$className.java --data=/data
-  aws s3 cp --no-progress /data/$THEME.pmtiles s3://$BUCKET/$RELEASE_TILESET/$THEME.pmtiles
+  aws s3 cp --no-progress /data/$THEME.pmtiles s3://$DEST_BUCKET_FOLDER/$THEME.pmtiles
 fi
